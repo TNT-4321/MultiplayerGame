4efa3b3daea195b0d8fc02eef09f3cd92098a068
TNT-4321 <129206466+TNT-4321@users.noreply.github.com>
2023-04-08 17:19:59 +0200

    SaveManager(V1)

---
+++ GitHubProject/Assets/Scripts/SaveAndLoad/SaveManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance {get; private set;}
+
+    private void Awake() 
+    {
+        //Make sure that this intance never exists twice
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            //We set the Instance to this script and now we can access it from everywhere
+            Instance = this;
+            //That the object that this script is attached to does not get destroyed if we load a new scene 
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SaveAsPrefab(GameObject gameObjectToBecomePrefab)
+    {
+        string localPath = "Assets/Prefabs" + gameObjectToBecomePrefab.name + ".prefab";
+
+        //Make sure the file name is unique, in case an existing prefab has the same name 
+        localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+
+        //Create the new prefab 
+        PrefabUtility.SaveAsPrefabAssetAndConnect(gameObjectToBecomePrefab, localPath, InteractionMode.UserAction);
+    }
+}
